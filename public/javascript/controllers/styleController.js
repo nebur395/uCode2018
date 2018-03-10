376f095b25cde8c0badbd42b97da506cf2c21ecc
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-03-10 18:25:30 +0100

    Test to check div heights

--- public/javascript/controllers/styleController.js
+++ public/javascript/controllers/styleController.js
@@ -11,10 +11,18 @@ angular.module('ucode18')
         $scope.logosList = [
             {id:0,src:"../images/style/clothes/camisetalogogrande.jpg", price: 20.5, selected: false},
             {id:1,src:"../images/style/clothes/camisetalogopequeno.jpg", price: 36.5, selected: false}];
+        $scope.totalClothes = 0;
 
         $scope.activeCloth = function(id) {
             for (i = 0; i < $scope.clothesList.length; i++) {
-                $scope.clothesList[i].selected = id === $scope.clothesList[i].id;
+                $scope.clothesList[i].selected = (id === $scope.clothesList[i].id)
+                    ? !$scope.clothesList[i].selected : $scope.clothesList[i].selected;
+
+                if ((id === $scope.clothesList[i].id) && $scope.clothesList[i].selected) {
+                    $scope.totalClothes += $scope.clothesList[i].price;
+                } else if ((id === $scope.clothesList[i].id) && !$scope.clothesList[i].selected) {
+                    $scope.totalClothes -= $scope.clothesList[i].price;
+                }
             }
         }
 
