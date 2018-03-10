658cbc550e50cb32e6e61496d044cf9a60227003
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-03-10 17:17:57 +0100

    Added accordions

--- public/javascript/controllers/styleController.js
+++ public/javascript/controllers/styleController.js
@@ -3,10 +3,19 @@ angular.module('ucode18')
     .controller('styleCtrl', ['$scope', '$state', function ($scope) {
 
         $scope.clothesList = [
-            {}];
+            {id:0,src:"../images/style/clothes/camisetabase.jpg", price: 20.5, selected: false},
+            {id:1,src:"../images/style/clothes/camisetabase.jpg", price: 36.5, selected: false}];
+        $scope.stylesList = [
+            {id:0,src:"../images/style/clothes/camisetabasenegro.jpg", price: 20.5, selected: false},
+            {id:1,src:"../images/style/clothes/camisetalrayas.jpg", price: 36.5, selected: false}];
+        $scope.logosList = [
+            {id:0,src:"../images/style/clothes/camisetalogogrande.jpg", price: 20.5, selected: false},
+            {id:1,src:"../images/style/clothes/camisetalogopequeno.jpg", price: 36.5, selected: false}];
 
-        $scope.getNumber = function(num) {
-            return new Array(num);
+        $scope.activeCloth = function(id) {
+            for (i = 0; i < $scope.clothesList.length; i++) {
+                $scope.clothesList[i].selected = id === $scope.clothesList[i].id;
+            }
         }
 
     }]);
