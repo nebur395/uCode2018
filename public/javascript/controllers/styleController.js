c7ef11b74befba9a7c4a96303889534741f69152
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-03-11 01:04:46 +0100

    improved cards selected style

--- public/javascript/controllers/styleController.js
+++ public/javascript/controllers/styleController.js
@@ -3,13 +3,16 @@ angular.module('ucode18')
     .controller('styleCtrl', ['$scope', '$state', function ($scope) {
 
         $scope.styleImageView = "../images/style/view/original.jpg";
-        $scope.state = 000;
+
+        $scope.showClothCard = false;
+        $scope.showStyleCard = false;
+        $scope.showLogoCard = false;
 
         $scope.clothesList = [
             {id:0,src:"../images/style/clothes/camisetabase.jpg", price: 20.5, selected: false, model: "XX1"},
-            {id:0,src:"../images/style/clothes/camisetabasenegro.jpg", price: 1, selected: false, model: "XX3"}];
-        $scope.stylesList = [,
-            {id:1,src:"../images/style/clothes/camisetalrayas.jpg", price: 1, selected: false, model: "XX4"}];
+            {id:1,src:"../images/style/clothes/camisetabasenegro.jpg", price: 1, selected: false, model: "XX3"}];
+        $scope.stylesList = [
+            {id:0,src:"../images/style/clothes/camisetalrayas.jpg", price: 1, selected: false, model: "XX4"}];
         $scope.logosList = [
             {id:0,src:"../images/style/clothes/camisetalogogrande.jpg", price: 5, selected: false, model: "XX4"},
             {id:1,src:"../images/style/clothes/camisetalogopequeno.jpg", price: 1, selected: false, model: "XX5"}];
@@ -56,6 +59,26 @@ angular.module('ucode18')
                 } else {
                     $scope.logosList[i].selected = false;
                 }
+            }
+        };
+
+        $scope.openCard = function(card) {
+            switch (card) {
+                case 1:
+                    $scope.showClothCard = !$scope.showClothCard;
+                    $scope.showStyleCard = false;
+                    $scope.showLogoCard = false;
+                    break;
+                case 2:
+                    $scope.showClothCard = false;
+                    $scope.showStyleCard = !$scope.showStyleCard;
+                    $scope.showLogoCard = false;
+                    break;
+                case 3:
+                    $scope.showClothCard = false;
+                    $scope.showStyleCard = false;
+                    $scope.showLogoCard = !$scope.showLogoCard;
+                    break;
             }
         };
 
